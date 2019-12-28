fca8fd2417fe8caccf9d1332f0253e9a016ebd21
shadab <ks615044@gmail.com>
2019-12-28 11:22:40 -0800

    Title Changes

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 					?>				
 					<?php include 'includes/header.php'; ?>
 		<head>
-			<title>Ecomm- ONLINE SHOPPING WEBSITE</title>
+			<title>Ecomm- ONLINE SHOPPING Portal</title>
 		<link rel="stylesheet" href="build/swiper.min.css">
 
 		</head>

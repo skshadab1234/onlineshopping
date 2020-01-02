1aa159fd21f0c301a6436c73e9fc88e282cb8d8f
unknown <>
2020-01-02 09:03:47 +0530

    edited

--- includes/header.php
+++ includes/header.php
@@ -1126,7 +1126,7 @@ padding: 15px;
 
 <script>
 // Set the date we're counting down to
-var countDownDate = new Date("jan 2, 2020 09:00:00 AM").getTime();
+var countDownDate = new Date("jan 3, 2020 09:00:00 AM").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
 

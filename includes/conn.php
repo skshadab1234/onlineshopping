f563f15a181ac12bd16a06800c61ac7174ec9786
unknown <>
2020-02-17 14:11:29 +0530

    Connection

--- includes/conn.php
+++ includes/conn.php
@@ -3,7 +3,7 @@
 class Database
 {
 
-	private $server = "pgsql:host=ec2-46-137-177-160.eu-west-1.compute.amazonaws.com;port:5432;dbname=d905fbous6e6gr";
+	private $server = "pgsql:host=ec2-46-137-177-160.eu-west-1.compute.amazonaws.com;port=5432;dbname=d905fbous6e6gr";
 	private $username = "jwudrsfkfrpmvh";
 	private $password = "e3e08fd3b4c54f9b0c82eeca7b051d631d90bd31d972bea4d6ea405b77ddda9f";
 	private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
